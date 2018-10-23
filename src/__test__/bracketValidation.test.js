8b8bf69996a2d79d752683c37cec0b36569c2c40
Kris3579 <kristianesvelt@hotmail.com>
2018-10-22 17:39:54 -0700

    done

--- src/__test__/bracketValidation.test.js
+++ src/__test__/bracketValidation.test.js
@@ -1,34 +1,22 @@
 'use strict';
 
-const isItAChild = require('../isItAChild');
-const Node = require('../lib/Node');
-const Tree = require('../lib/Tree');
+const BracketValidation = require('../bracketValidation');
 
-const testTree = new Tree(10);
-testTree.root.left = new Node(1);
-testTree.root.left.left = new Node(9);
-testTree.root.left.right = new Node(3);
-testTree.root.right = new Node(5);
-testTree.root.right.right = new Node(7);
-testTree.root.right.left = new Node(4);
-testTree.root.right.left.left = new Node(100);
-testTree.root.right.left.right = new Node(11);
-
-describe('repeatedWord.js', () => {
-  test('Successfully returned false when checking if 100 is a child of 9', () => {
-    const result = isItAChild(testTree.root, 9, 100);
-    expect(result).toEqual(false);
-  });
-  test('Successfully returned false when checking if 4 is a child of 1', () => {
-    const result = isItAChild(testTree.root, 1, 4);
-    expect(result).toEqual(false);
-  });
-  test('Successfully returned false when checking if 11 is a child of 10', () => {
-    const result = isItAChild(testTree.root, 10, 11);
+describe('bracketValidation.js', () => {
+  test('Returned true if valid', () => {
+    const result = BracketValidation('({[]})');
     expect(result).toEqual(true);
   });
-  test('Successfully returned false when checking if 7 is a child of 5', () => {
-    const result = isItAChild(testTree.root, 5, 7);
-    expect(result).toEqual(true);
+  test('Returned false if type other than string', () => {
+    const result = BracketValidation({ thing: 'stuff' });
+    expect(result).toEqual(false);
+  });
+  test('Returned false if to many brackets', () => {
+    const result = BracketValidation('[[()]');
+    expect(result).toEqual(false);
+  });
+  test('Returned false if to little brackets', () => {
+    const result = BracketValidation('[({)]');
+    expect(result).toEqual(false);
   });
 });
